be042b2376807a3c8558f928819a229c2716811b
letov7482-ui <letov7482@gmail.com>
2026-07-28 13:32:53 +0300

    Update SpawnerESP.java

--- src/main/java/com/skywhy/module/modules/SpawnerESP.java
+++ src/main/java/com/skywhy/module/modules/SpawnerESP.java
@@ -1,13 +1,15 @@
 package com.skywhy.module.modules;
+
 import com.skywhy.module.Module;
 import com.skywhy.render.Render3D;
-import net.minecraft.block.entity.SpawnerBlockEntity;
+import net.minecraft.block.entity.MobSpawnerBlockEntity;
+
 public class SpawnerESP extends Module {
     public SpawnerESP() { super("SpawnerESP", Category.VISUAL); }
     @Override
     public void onRender3D() {
         if (mc.world == null) return;
-        for (SpawnerBlockEntity spawner : mc.world.getBlockEntities(SpawnerBlockEntity.class)) {
+        for (MobSpawnerBlockEntity spawner : mc.world.getBlockEntities(MobSpawnerBlockEntity.class)) {
             double x = spawner.getPos().getX(), y = spawner.getPos().getY(), z = spawner.getPos().getZ();
             Render3D.drawESPBox(x-0.5, y-0.5, z-0.5, 1, 1, 1, 0xFF00FF, 0.4f);
         }

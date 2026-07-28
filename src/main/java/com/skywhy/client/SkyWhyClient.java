2a89b4a584dd9faa0a54b158862361fc0448c228
letov7482-ui <letov7482@gmail.com>
2026-07-28 11:01:06 +0300

    Update SkyWhyClient.java

--- src/main/java/com/skywhy/client/SkyWhyClient.java
+++ src/main/java/com/skywhy/client/SkyWhyClient.java
@@ -1,14 +1,16 @@
 package com.skywhy.client;
 
+import com.skywhy.module.ModuleManager;
+import com.skywhy.config.ConfigManager;
+import com.skywhy.hud.HUDManager;
+import com.skywhy.utils.AntiBanSystem;
+import com.skywhy.gui.ClickGUI;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
-import com.skywhy.module.ModuleManager;
-import com.skywhy.config.ConfigManager;
-import com.skywhy.hud.HUDManager;
 
 public class SkyWhyClient implements ClientModInitializer {
     public static final String MOD_ID = "skywhy";
@@ -25,16 +27,25 @@ public class SkyWhyClient implements ClientModInitializer {
         moduleManager = new ModuleManager();
         configManager = new ConfigManager();
         hudManager = new HUDManager();
+
         clickGuiKey = new KeyBinding("key.skywhy.clickgui", GLFW.GLFW_KEY_RSHIFT, "SkyWhy Client");
         KeyBindingHelper.registerKeyBinding(clickGuiKey);
+
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (clickGuiKey.wasPressed()) {
-                // Open ClickGUI
-                // client.setScreen(new ClickGUIScreen());
+                if (client.currentScreen instanceof ClickGUI) {
+                    client.setScreen(null);
+                } else {
+                    client.setScreen(new ClickGUI());
+                }
             }
             moduleManager.onTick();
+            AntiBanSystem.bypass(); // ПОСТОЯННЫЙ БАЙПАСС АНТИЧИТА
         });
+
         HudRenderCallback.EVENT.register((matrixStack, tickDelta) -> hudManager.render(matrixStack, tickDelta));
         configManager.load();
+
+        System.out.println("[SkyWhy] Client initialized with full bypass and cosmetics!");
     }
 }

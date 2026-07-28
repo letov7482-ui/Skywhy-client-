14148a37cb620c6909a96e0e5fe6f90c16299870
letov7482-ui <letov7482@gmail.com>
2026-07-28 13:33:46 +0300

    Update AutoEat.java

--- src/main/java/com/skywhy/module/modules/AutoEat.java
+++ src/main/java/com/skywhy/module/modules/AutoEat.java
@@ -1,8 +1,11 @@
 package com.skywhy.module.modules;
+
 import com.skywhy.module.Module;
-import net.minecraft.item.FoodComponent;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
+
 public class AutoEat extends Module {
     public AutoEat() { super("AutoEat", Category.PLAYER); }
     @Override
@@ -11,7 +14,7 @@ public class AutoEat extends Module {
         if (mc.player.getHungerManager().getFoodLevel() < 10) {
             for (int i = 0; i < 9; i++) {
                 ItemStack stack = mc.player.getInventory().getStack(i);
-                if (stack.getItem().getFoodComponent() != null) {
+                if (stack.contains(DataComponentTypes.FOOD)) {
                     mc.player.getInventory().selectedSlot = i;
                     mc.options.useKey.setPressed(true);
                     break;

fa36cde6577f6d77a3489beaf5b8eedcbdc72382
letov7482-ui <letov7482@gmail.com>
2026-07-28 09:56:45 +0300

    Update LongJump.java

--- src/main/java/com/skywhy/module/modules/LongJump.java
+++ src/main/java/com/skywhy/module/modules/LongJump.java
@@ -1,3 +1,14 @@
 package com.skywhy.module.modules;
 import com.skywhy.module.Module;
-public class LongJump extends Module { public LongJump() { super("LongJump", Category.MOVEMENT); } }
+public class LongJump extends Module {
+    public LongJump() { super("LongJump", Category.MOVEMENT); }
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+        if (mc.player.isOnGround()) {
+            mc.player.jump();
+            mc.player.getVelocity().x *= 1.5;
+            mc.player.getVelocity().z *= 1.5;
+        }
+    }
+}

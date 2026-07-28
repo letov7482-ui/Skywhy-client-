dede048aac1529433f0349ba56a9a0130f61fb1d
letov7482-ui <letov7482@gmail.com>
2026-07-28 09:54:34 +0300

    Update Velocity.java

--- src/main/java/com/skywhy/module/modules/Velocity.java
+++ src/main/java/com/skywhy/module/modules/Velocity.java
@@ -1,3 +1,15 @@
 package com.skywhy.module.modules;
 import com.skywhy.module.Module;
-public class Velocity extends Module { public Velocity() { super("Velocity", Category.COMBAT); } }
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
+public class Velocity extends Module {
+    private float horizontalReduction = 0.8f;
+    private float verticalReduction = 0.6f;
+    public Velocity() { super("Velocity", Category.COMBAT); }
+    @Override
+    public void onTick() {
+        if (mc.player == null) return;
+        Vec3d vel = mc.player.getVelocity();
+        mc.player.setVelocity(vel.x * horizontalReduction, vel.y * verticalReduction, vel.z * horizontalReduction);
+    }
+}

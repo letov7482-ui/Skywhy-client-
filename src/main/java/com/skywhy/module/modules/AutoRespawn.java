8cac2e36b3278810a0be6ba89b765fb99befb1db
letov7482-ui <letov7482@gmail.com>
2026-07-28 09:57:37 +0300

    Update AutoRespawn.java

--- src/main/java/com/skywhy/module/modules/AutoRespawn.java
+++ src/main/java/com/skywhy/module/modules/AutoRespawn.java
@@ -1,3 +1,12 @@
 package com.skywhy.module.modules;
 import com.skywhy.module.Module;
-public class AutoRespawn extends Module { public AutoRespawn() { super("AutoRespawn", Category.PLAYER); } }
+import net.minecraft.client.network.ClientPlayerEntity;
+public class AutoRespawn extends Module {
+    public AutoRespawn() { super("AutoRespawn", Category.PLAYER); }
+    @Override
+    public void onTick() {
+        if (mc.player != null && mc.player.isDead()) {
+            mc.player.requestRespawn();
+        }
+    }
+}
